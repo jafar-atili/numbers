1b239d70fa2565d4a680ca00230a27effba70374
Jafar Atili <at.jafar@outlook.com>
2016-06-04 19:59:12 +0300

    Added support to enter few numbers in the same line (separated by spaces) for both input files and standard input

--- numbers.c
+++ numbers.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 #define OK 0
-#define MAX_LINE 5
-#define MAX_CHAR 100
+#define MAX_LINE 1000
+#define MAX_CHAR 1000
 
 
 char * translate_number (int x);
@@ -20,90 +20,85 @@ int main (int argc, char *argv[])
 	int number;
 	/*output: will hold a pointer of the translated string output*/
 	char *output;
-	/*c: a character to be entered - meant to recieve EOF*/
-	char c;
-	/*line: i a line in the input file (ifp)*/
-	char line[MAX_LINE];
-	/*the number of command line argument recieved*/
+	/*the number of command line argument received*/
 	int num_of_arguments = argc-1;
 
 
-	/*this switch case will start the relevant sub program based on the number of argument we recienved*/
+
+	/*this switch case will start the relevant sub program based on the number of argument we received*/
 	switch (num_of_arguments) {
 		/*2 arguments: the first one will be an input file
 		the second one will be the output file*/
-		case 2 : 
+		case 2 :
 			/*if we were not able to open the input file in read mode*/
 			if ((ifp=fopen(argv[1], "r")) == NULL)
 				fprintf(stderr ,"Cannot Open file %s\n", argv[1]);
-			/*if we were not able to open the output file in write mode*/
+				/*if we were not able to open the output file in write mode*/
 			else if ((ofp=fopen(argv[2], "w"))== NULL)
 				fprintf(stderr, "Cannot Open file %s\n", argv[2]);
-			/*we succeded to open both files*/
-			else 
+				/*we succeeded to open both files*/
+			else
 			{
-				/*while the input file still has line*/
-				while (fgets(line, MAX_LINE, ifp)!= NULL)
+				/*scan numbers from input file to number until reaching EOF*/
+				while (fscanf(ifp, "%d", &number) != EOF)
 				{
-				/*replance the enter \n with \0 as string termination*/
-				*(strchr(line, '\n'))='\0';
-					/*convert the line to integer and assigne to number*/
-					number = atoi (line);
-					/*translate the number to a string and assigne to output*/
-					output = translate_number (atoi(line));
-					/*write output to the ouptu file*/
+					/*assign the output of translate_number to output*/
+					output = translate_number (number);
+					/*print the output which contains the translated number to the output file*/
 					fprintf(ofp, "%s\n", output);
 				}
 			}
 			/*after we done, close both files*/
 			fclose(ifp);
 			fclose(ofp);
-		break;
+			break;
 
-		/*we recieved only one file, this will be the input file, the output will
-		be redirected to the standart output*/
-		case 1 : 
-			/*Similiar to "case 2", instead writing the output to an output file
-			we printing it to the standart output*/
+			/*we received only one file, this will be the input file, the output will
+            be redirected to the standard output*/
+		case 1 :
+			/*Similar to "case 2", instead writing the output to an output file
+			we printing it to the standard output*/
 			if ((ifp=fopen(argv[1], "r")) == NULL)
 				fprintf(stderr, "Cannot Open file %s\n", argv[1]);
-			else 
+			else
 			{
-				while (fgets(line, MAX_LINE, ifp)!= NULL)
+				/*scan numbers from input file to number until reaching EOF*/
+				while (fscanf(ifp, "%d", &number) != EOF)
 				{
-					*(strchr(line, '\n'))='\0';
-					number = atoi (line);
-					output = translate_number (atoi(line));
+					/*assign the output of translate_number to output*/
+					output = translate_number (number);
+					/*print the output to the standard output*/
 					printf("%s\n", output);
 				}
 			}
+			/*close file*/
 			fclose(ifp);
-		break;
+			break;
 
-		/*we didnt recieve output and input files, the input number will be taken
-		directly from the standart input and the output will be redirected to the 
-		standart output*/
-		case 0 : printf("Enter a number to be translated:\n");
-			do 
-			{	
-				/*scan the number with scanf and assign the value to number variable*/
-				scanf ("%i", &number);
+			/*we didn't received output and input files, the input number will be taken
+            directly from the standard input and the output will be redirected to the
+            standard output*/
+		case 0 : printf("\nUsage:  Hitting Enter will show the result and wait for another input.\n\tCtrl-D will terminate the program.\n\tEntering Characters is not supported!\n\nEnter numbers to be translated:\n");
+
+
+			/*scan numbers into c while no End-Of-File received*/
+			while (scanf("%d", &number) != EOF)
+			{
 				/*if translate return a 0 value, this means that the entered number is >= 100 and number is < 0*/
 				if (*translate_number(number) == '0')
-					/*redirect the error message to the system's standart error*/
+					/*redirect the error message to the system's standard error*/
 					fprintf(stderr, "Not Supported Number\n" );
 				else
 					/*translate didn't retrun '0', then print the string*/
 					printf("%s\n", translate_number(number));
-				/*loop while we didn't recieve EOF*/
-			} while ((c = getchar()) != EOF);
-		break;
+			}
+			break;
 
-		/*we rcieved more that 2 arguments, print the usage*/
+			/*we received more that 2 arguments, print the usage*/
 		default : fprintf(stderr, "Not Supported\nUsage:  ./numbers <input file> <output file>\nOr\t./numbers <input file>\nOr\t./numbers\n");
-		break;
+			break;
 	}
-return OK;	
+	return OK;
 }
 
 
@@ -116,29 +111,29 @@ char * translate_number(int x){
 	char *ptr;
 	/*single digit numbers in english*/
 	char *single_digit_number[]={"zero", "one","two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
-	/*number in englesh when the second digit of a number is zero*/
+	/*number in english when the second digit of a number is zero*/
 	char *second_digit_is_zero[]={"0","", "twenty ", "thirty ", "fourty ", "fifty ", "sixty ", "sevnety ", "eighty ", "ninty "};
 	/*the number in english when the first digit is one*/
 	char *first_digit_is_one[]={"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","ninteen","twenty"};
 	ptr = output_string;
 
 
-	/*if the number between 0-9, return the name of it which located in the number idex in single_digit_number array*/
+	/*if the number between 0-9, return the name of it which located in the number index in single_digit_number array*/
 	if (x >= 0 && x < 10)
 		return single_digit_number[x];
-	/*if number between 9-99 but the first digit is one*/
+		/*if number between 9-99 but the first digit is one*/
 	else if(x > 9 && x < 100 && x/10 == 1)
 		return first_digit_is_one[x%10];
-	/*if the number between 9-99 but the second digit is zero*/
+		/*if the number between 9-99 but the second digit is zero*/
 	else if (x > 9 && x < 100 && x%10 == 0)
 		return second_digit_is_zero[x/10];
-	/*if a number that the second digit is not zero and the first digit is not one,
-	it result in english as a combination between the both arrays (second_digit_is_zero and single_digit_number)*/
+		/*if a number that the second digit is not zero and the first digit is not one,
+        it result in english as a combination between the both arrays (second_digit_is_zero and single_digit_number)*/
 	else if(x > 9 && x < 100 && x%10 != 0 && x/10 !=1)
 	{
 		/*copy the right string from the first array to the output_string*/
 		strcpy (output_string, second_digit_is_zero[x/10]);
-		/*concatante the right string to it from the right array*/
+		/*concatenate the right string to it from the right array*/
 		strcat (output_string, single_digit_number[x%10]);
 		/*return a pointer of the ouput_string*/
 		return ptr;
